3191fb7bcc7828517d62682400d488f0a48ea173
sting741 <167591747+sting741@users.noreply.github.com>
2024-04-20 08:44:32 +0700

    v1

---
+++ Lab1/src/main/java/com/mycompany/lab1/Lab1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab1;
+
+/**
+ *
+ * @author Student
+ */
+public class Lab1 {
+
+    public static void main(String[] args) {
+        Question1 q1 = new Question1(123456);
+        System.out.println(q1.SumFirstAndLastDigit());
+    }
+}
---
+++ Lab1/src/main/java/com/mycompany/lab1/Question1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab1;
+
+/**
+ *
+ * @author Student
+ */
+public class Question1 {
+    private int number;
+    public Question1(){
+        
+    }
+    public Question1(int number){
+        this.number = number;
+    }
+    
+    public int SumFirstAndLastDigit(){
+        int lastDigit = number%10;
+        int firstDigit = number;
+        while (firstDigit>=10) {            
+            firstDigit/=10;
+        
+        }
+        return lastDigit + firstDigit;
+    }
+}
